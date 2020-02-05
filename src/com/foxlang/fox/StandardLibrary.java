42fe50627d5af6215f7c6033c7aafee6ab663a35
mrpascal1 <ansarishahid640@gmail.com>
2020-02-05 20:34:53 +0530

    StandardLibrary.java fixed with correct syntax.

--- src/com/foxlang/fox/StandardLibrary.java
+++ src/com/foxlang/fox/StandardLibrary.java
@@ -7,32 +7,32 @@ public class StandardLibrary {
     public static final String File =
             "class File {\n" +
                 "init(path) {\n" +
-                    "this.path = path\n" +
+                    "this.path = path;\n" +
                 "}\n" +
                 "read() {\n" +
-                    "return readFile(this.path)\n" +
+                    "return readFile(this.path);\n" +
                 "}\n" +
                 "write(data) {\n" +
-                    "return writeFile(this.path, data)\n" +
+                    "return writeFile(this.path, data);\n" +
                 "}\n" +
                 "append(data) {\n" +
-                    "return appendFile(this.path, data)\n" +
+                    "return appendFile(this.path, data);\n" +
                 "}\n" +
             "}\n";
 
     public static final String Random =
-            "import \"std:Bitwise\"\n" +
-            "var _stdRandomSeed = clock()\n" +
+            "import \"std:Bitwise\";\n" +
+            "var _stdRandomSeed = clock();\n" +
             "class Random {\n" +
-                "class seed(number) {\n" +
-                    "_stdRandomSeed = number\n" +
+                "static seed(number) {\n" +
+                    "_stdRandomSeed = number;\n" +
                 "}\n" +
-                "class random() {\n" +
-                    "_stdRandomSeed = (1103515245 * _stdRandomSeed + 12345) % 2147483648\n" +
-                    "return Bitwise.rightShift(_stdRandomSeed, 0)\n" +
+                "static random() {\n" +
+                    "_stdRandomSeed = (1103515245 * _stdRandomSeed + 12345) % 2147483648;\n" +
+                    "return Bitwise.rightShift(_stdRandomSeed, 0);\n" +
                 "}\n" +
-                "class inRange(min, max) {\n" +
-                    "return min + round((max - min + 1) * num(Random.random()) * num(1 / 2147483648))\n" +
+                "static inRange(min, max) {\n" +
+                    "return min + round((max - min + 1) * num(Random.random()) * num(1 / 2147483648));\n" +
                 "}\n" +
             "}\n";
 
